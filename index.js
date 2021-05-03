ad452504e0e6babe46689544183cebf91a2ed51d
Aravind <43021733+aravindb12@users.noreply.github.com>
2021-05-03 22:41:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+(function () {
+  var scrollContainer = document.getElementsByClassName("scroll-container")[0];
+  window.addEventListener(
+    "wheel",
+    function (event) {
+      if (event.wheelDeltaY) {
+        if (Math.abs(event.wheelDeltaY) > 120) {
+          event.preventDefault();
+          currentScrollPos = $(".scroll-container").scrollTop();
+          if (event.deltaY > 0) {
+            $(".scroll-container").scrollTop(
+              currentScrollPos + $(window).height()
+            );
+          } else {
+            $(".scroll-container").scrollTop(
+              currentScrollPos - $(window).height()
+            );
+          }
+          event.stopPropagation();
+        }
+      }
+    },
+    { passive: false }
+  );
+
+  scrollContainer.addEventListener("scroll", function () {
+    $("section").each(function () {
+      var sectionPos = $(this).offset().top;
+      var sectionHeight = $(this).height();
+      var topOfWindow = $(window).scrollTop();
+
+      if (
+        sectionPos < topOfWindow + sectionHeight &&
+        sectionPos + sectionHeight > topOfWindow
+      ) {
+        $(this).addClass("can-slide");
+      } else {
+        $(this).removeClass("can-slide");
+      }
+    });
+  });
+
+  animateDiv();
+
+  function animateDiv() {
+
+    $(".floating-shapes").each(function () {
+      var newq = makeNewPosition();
+      $(this).animate({ top: newq[0], left: newq[1] }, 5000, function () {
+        animateDiv();
+      });
+    });
+  }
+
+  function makeNewPosition() {
+    var h = $(window).height() - 20;
+    var w = $(window).width() - 20;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+  }
+  $("#intro").addClass("can-slide");
+})();
