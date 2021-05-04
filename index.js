1b08c3d3364160016d3a513ef70dca61852129ea
Aravind <43021733+aravindb12@users.noreply.github.com>
2021-05-04 18:38:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
-  var scrollContainer = document.getElementsByClassName("scroll-container")[0];
-  window.addEventListener(
-    "wheel",
-    function (event) {
-      if (event.wheelDeltaY) {
-        if (Math.abs(event.wheelDeltaY) > 120) {
-          event.preventDefault();
-          currentScrollPos = $(".scroll-container").scrollTop();
-          if (event.deltaY > 0) {
-            $(".scroll-container").scrollTop(
-              currentScrollPos + $(window).height()
-            );
-          } else {
-            $(".scroll-container").scrollTop(
-              currentScrollPos - $(window).height()
-            );
-          }
-          event.stopPropagation();
-        }
-      }
-    },
-    { passive: false }
-  );
+   var scrollContainer = document.getElementsByClassName("scroll-container")[0];
+//   window.addEventListener(
+//     "wheel",
+//     function (event) {
+//       if (event.wheelDeltaY) {
+//         if (Math.abs(event.wheelDeltaY) > 120) {
+//           event.preventDefault();
+//           currentScrollPos = $(".scroll-container").scrollTop();
+//           if (event.deltaY > 0) {
+//             $(".scroll-container").scrollTop(
+//               currentScrollPos + $(window).height()
+//             );
+//           } else {
+//             $(".scroll-container").scrollTop(
+//               currentScrollPos - $(window).height()
+//             );
+//           }
+//           event.stopPropagation();
+//         }
+//       }
+//     },
+//     { passive: false }
+//   );
 
   scrollContainer.addEventListener("scroll", function () {
     $("section").each(function () {
