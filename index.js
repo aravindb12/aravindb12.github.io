1101391e48504e20484cc03918d73157694957d5
Aravind <43021733+aravindb12@users.noreply.github.com>
2021-05-04 19:09:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 (function () {
-  const rootElement = document.querySelector(".scroll-container")
-const viewPortH = rootElement.getBoundingClientRect().height;
-const windowH = window.innerHeight;
-const browserUiBarsH = viewPortH - windowH;
-rootElement.style.height = `calc(100vh - ${browserUiBarsH}px)`;
-   var scrollContainer = document.getElementsByClassName("scroll-container")[0];
-//   window.addEventListener(
-//     "wheel",
-//     function (event) {
-//       if (event.wheelDeltaY) {
-//         if (Math.abs(event.wheelDeltaY) > 120) {
-//           event.preventDefault();
-//           currentScrollPos = $(".scroll-container").scrollTop();
-//           if (event.deltaY > 0) {
-//             $(".scroll-container").scrollTop(
-//               currentScrollPos + $(window).height()
-//             );
-//           } else {
-//             $(".scroll-container").scrollTop(
-//               currentScrollPos - $(window).height()
-//             );
-//           }
-//           event.stopPropagation();
-//         }
-//       }
-//     },
-//     { passive: false }
-//   );
+  const rootElement = document.querySelector(".scroll-container");
+  const viewPortH = rootElement.getBoundingClientRect().height;
+  const windowH = window.innerHeight;
+  const browserUiBarsH = viewPortH - windowH;
+  rootElement.style.height = `calc(100vh - ${browserUiBarsH}px)`;
+  var scrollContainer = document.getElementsByClassName("scroll-container")[0];
+  window.addEventListener(
+    "wheel",
+    function (event) {
+      if (event.wheelDeltaY) {
+        if (Math.abs(event.wheelDeltaY) > 120) {
+          event.preventDefault();
+          currentScrollPos = $(".scroll-container").scrollTop();
+          const rootElement = document.querySelector(".scroll-container");
+          const viewPortH = rootElement.getBoundingClientRect().height;
+          const windowH = window.innerHeight;
+          const browserUiBarsH = viewPortH - windowH;
+          if (event.deltaY > 0) {
+            $(".scroll-container").scrollTop(
+              currentScrollPos + $(window).height()-browserUiBarsH
+            );
+          } else {
+            $(".scroll-container").scrollTop(
+              currentScrollPos - $(window).height()-browserUiBarsH
+            );
+          }
+          event.stopPropagation();
+        }
+      }
+    },
+    { passive: false }
+  );
 
   scrollContainer.addEventListener("scroll", function () {
     $("section").each(function () {
@@ -48,7 +52,6 @@ rootElement.style.height = `calc(100vh - ${browserUiBarsH}px)`;
   animateDiv();
 
   function animateDiv() {
-
     $(".floating-shapes").each(function () {
       var newq = makeNewPosition();
       $(this).animate({ top: newq[0], left: newq[1] }, 5000, function () {
