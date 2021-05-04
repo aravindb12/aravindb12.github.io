0682cdb3b617ec5ece702b062a7decfa23137f73
Aravind <43021733+aravindb12@users.noreply.github.com>
2021-05-04 19:23:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 (function () {
-  const rootElement = document.querySelector(".scroll-container");
-  const viewPortH = rootElement.getBoundingClientRect().height;
-  const windowH = window.innerHeight;
-  const browserUiBarsH = viewPortH - windowH;
-  rootElement.style.height = `calc(100vh - ${browserUiBarsH}px)`;
-  var scrollContainer = document.getElementsByClassName("scroll-container")[0];
+  var scrollContainer = document.querySelector(".scroll-container");
+  var browserUiBarsH;
+  function getActualHeight() {
+    const rootElement = document.querySelector(".scroll-container");
+    const viewPortH = rootElement.getBoundingClientRect().height;
+    const windowH = window.innerHeight;
+    return (browserUiBarsH = viewPortH - windowH);
+  }
+
   window.addEventListener(
     "wheel",
     function (event) {
@@ -12,17 +15,13 @@
         if (Math.abs(event.wheelDeltaY) > 120) {
           event.preventDefault();
           currentScrollPos = $(".scroll-container").scrollTop();
-          const rootElement = document.querySelector(".scroll-container");
-          const viewPortH = rootElement.getBoundingClientRect().height;
-          const windowH = window.innerHeight;
-          const browserUiBarsH = viewPortH - windowH;
           if (event.deltaY > 0) {
             $(".scroll-container").scrollTop(
-              currentScrollPos + $(window).height()-browserUiBarsH
+              currentScrollPos + $(window).height() - getActualHeight()
             );
           } else {
             $(".scroll-container").scrollTop(
-              currentScrollPos - $(window).height()-browserUiBarsH
+              currentScrollPos - $(window).height() - getActualHeight()
             );
           }
           event.stopPropagation();
@@ -49,25 +48,6 @@
     });
   });
 
-  animateDiv();
-
-  function animateDiv() {
-    $(".floating-shapes").each(function () {
-      var newq = makeNewPosition();
-      $(this).animate({ top: newq[0], left: newq[1] }, 5000, function () {
-        animateDiv();
-      });
-    });
-  }
-
-  function makeNewPosition() {
-    var h = $(window).height() - 20;
-    var w = $(window).width() - 20;
-
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-
-    return [nh, nw];
-  }
   $("#intro").addClass("can-slide");
+  scrollContainer.style.height = `calc(100vh - ${getActualHeight()}px)`;
 })();
