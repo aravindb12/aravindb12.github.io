2b9a7e13f81f0b8470aa1d8598b9ee365ce65580
Aravind <43021733+aravindb12@users.noreply.github.com>
2021-05-25 19:03:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,5 @@
 (function () {
-  var scrollContainer = document.querySelector(".scroll-container");
-  var browserUiBarsH;
-  function getActualHeight() {
-    const rootElement = document.querySelector(".scroll-container");
-    const viewPortH = rootElement.getBoundingClientRect().height;
-    const windowH = window.innerHeight;
-    return (browserUiBarsH = viewPortH - windowH);
-  }
-
-  window.addEventListener(
-    "wheel",
-    function (event) {
-      if (event.wheelDeltaY) {
-        if (Math.abs(event.wheelDeltaY) > 120) {
-          event.preventDefault();
-          currentScrollPos = $(".scroll-container").scrollTop();
-          if (event.deltaY > 0) {
-            $(".scroll-container").scrollTop(
-              currentScrollPos + $(window).height() - getActualHeight()
-            );
-          } else {
-            $(".scroll-container").scrollTop(
-              currentScrollPos - $(window).height() - getActualHeight()
-            );
-          }
-          event.stopPropagation();
-        }
-      }
-    },
-    { passive: false }
-  );
-
-  scrollContainer.addEventListener("scroll", function () {
-    $("section").each(function () {
-      var sectionPos = $(this).offset().top;
-      var sectionHeight = $(this).height();
-      var topOfWindow = $(window).scrollTop();
-
-      if (
-        sectionPos < topOfWindow + sectionHeight &&
-        sectionPos + sectionHeight > topOfWindow
-      ) {
-        $(this).addClass("can-slide");
-      } else {
-        $(this).removeClass("can-slide");
-      }
-    });
-  });
-
-  $("#intro").addClass("can-slide");
-  scrollContainer.style.height = `calc(100vh - ${getActualHeight()}px)`;
+   $("#intro").addClass("can-slide");
 
   const monthNames = [
     "JANUARY",
@@ -65,20 +15,26 @@
     "NOVEMBER",
     "DECEMBER",
   ];
-  
+
   var date = new Date();
   document.querySelector("#date").innerText = ("0" + date.getDate()).slice(-2);
   document.querySelector("#month").innerText = monthNames[date.getMonth()];
 
+  var contact = document.querySelector("#click-contact");
+  contact.addEventListener("click", function(){
+    window.scrollTo(0,document.body.scrollHeight);
+  })
+
   var mode = document.querySelector(".mode");
   mode.addEventListener("click", function () {
     var body = $("body");
-    if(body.hasClass('light')){
-      $("body").removeClass("light");
-      mode.innerText = "Light";
-    } else {
-      $("body").addClass("light");
+    if (body.hasClass("dark")) {
+      $("body").removeClass("dark");
       mode.innerText = "Dark";
+    } else {
+      $("body").addClass("dark");
+      mode.innerText = "Light";
     }
   });
+
 })();
