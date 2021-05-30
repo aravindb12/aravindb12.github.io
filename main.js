49888f8f160262755edd60d9a1b4f1b5e9b87f8d
Aravind <aravindsbaskaran@gmail.com>
2021-05-30 12:28:38 +0530

    new minimalist ui update

---
+++ main.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("[data-toggle-sidebar]").forEach((toggle) => {
+    toggle.addEventListener("click", (e) => {
+      const sidebarID = toggle.dataset.toggleSidebar;
+      const sidebarElement = sidebarID
+        ? document.getElementById(sidebarID)
+        : undefined;
+      if (sidebarElement) {
+        let sidebarState = sidebarElement.getAttribute("aria-hidden");
+        sidebarElement.setAttribute("aria-hidden", sidebarState ? false : true);
+        document.querySelector("body").classList.add("side-active");
+      }
+    });
+  });
+
+  document.querySelectorAll(".sidebar__close").forEach((close) => {
+    close.addEventListener("click", (e) => {
+      e.currentTarget.parentElement.setAttribute("aria-hidden", true);
+      document.querySelector("body").classList.remove("side-active");
+    });
+  });
+
+  const cursor = document.querySelector(".cursor");
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+  });
+  const projectScroller = document.querySelector(".project-scroller");
+
+  document
+    .querySelector(".scroll-btn__next")
+    .addEventListener("click", scrollToNextPage);
+  document
+    .querySelector(".scroll-btn__prev")
+    .addEventListener("click", scrollToPrevPage);
+
+  var project_item_size;
+
+  function scrollToNextPage() {
+    project_item_size =
+      projectScroller.querySelector(".projects__item").clientWidth;
+    projectScroller.scrollBy({
+      top: 0,
+      left: project_item_size,
+      behavior: "smooth",
+    });
+  }
+
+  function scrollToPrevPage() {
+    project_item_size =
+      projectScroller.querySelector(".projects__item").clientWidth;
+    projectScroller.scrollBy({
+      top: 0,
+      left: -project_item_size,
+      behavior: "smooth",
+    });
+  }
+  const top_level_div = document.getElementById("land");
+  const divs = top_level_div.children;
+  var counter = 0;
+  const interval = window.setInterval(function () {
+    if (divs[counter - 1]) {
+      divs[counter - 1].classList.remove("show");
+    }
+
+    divs[counter].classList.add("show");
+    counter += 1;
+
+    if (counter === divs.length -1 ) {
+      window.clearInterval(interval);
+    }
+  }, 2000);
+});
